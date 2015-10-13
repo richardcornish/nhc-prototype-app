dffd7909e1bb45b8d074a4717f88853d27148a64
Richard Cornish <richardcornish@gmail.com>
2015-10-13 09:50:36 -0500

    Added patient list, patient detail, other progress

--- nhcprototype/static/js/main.js
+++ nhcprototype/static/js/main.js
@@ -1,1 +1,25 @@
+(function($){
 
+    // Make table rows clickable
+    $('.table').on('click', 'tr', function (e) {
+        if ($(e.target).is('a, input')) {
+            return;
+        }
+        location.href = $(this).find('a').attr('href');
+    });
+
+    // Format date widgets
+    $('[data-widget="date"]').datetimepicker({
+        format: 'MM/DD/YYYY',
+        viewMode: 'years'
+    });
+
+})(jQuery);
+
+
+(function($, FieldKit){
+
+    // Format phone inputs
+    new FieldKit.TextField($('[data-widget="phone"]').get(0), new FieldKit.PhoneFormatter());
+
+})(jQuery, FieldKit);
